2bfa5f8c992014104e385c3269adc0f5da5a0d63
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-06-13 23:02:18 +0300

    add operations and selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,4 @@
-export const getContacts = state => state.contacts;
-export const getFilter = state => state.filter;
+export const selectContactsList = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+export const selectContactsFilter = state => state.filters;
