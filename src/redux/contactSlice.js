c7eca280f7234ff0f0e99bab68539bf496f70c14
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-06-14 20:57:03 +0300

    add contactSlice

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,28 +1,58 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
-import contacts from '../components/contacts.json';
+import { createSlice } from '@reduxjs/toolkit';
+import { getContacts, addContact, deleteContact } from './operations';
+
+const pendingReducer = state => {
+  state.isLoading = false;
+};
+
+const rejectedReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const getContactsFullfieldReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items = action.payload;
+};
+
+const addContactFullfieldReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items.push(action.payload);
+  const index = state.itemsfindIndex(
+    contact => contact.id === action.payload.id
+  );
+  state.items.splice(index, 1);
+};
+
+const deleteContactFullfieldReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  const index = state.itemsfindIndex(
+    contact => contact.id === action.payload.id
+  );
+  state.items.splice(index, 1);
+};
 
 const contactSlice = createSlice({
   name: 'contacts',
-  initialState: contacts,
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        state.push(action.payload);
-      },
-      prepare(data) {
-        return {
-          payload: {
-            id: nanoid(),
-            ...data,
-          },
-        };
-      },
-    },
-    deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
-    },
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
   },
+  extraReducers: builder =>
+    builder
+      .addCase(getContacts.pending, pendingReducer)
+      .addCase(getContacts.fulfilled, getContactsFullfieldReducer)
+      .addCase(getContacts.rejected, rejectedReducer)
+      .addCase(addContact.pending, pendingReducer)
+      .addCase(addContact.fulfilled, addContactFullfieldReducer)
+      .addCase(addContact.rejected, rejectedReducer)
+      .addCase(deleteContact.pending, pendingReducer)
+      .addCase(deleteContact.fulfilled, deleteContactFullfieldReducer)
+      .addCase(deleteContact.rejected, rejectedReducer),
 });
 
-export const { addContact, deleteContact } = contactSlice.actions;
 export const contactReducer = contactSlice.reducer;
